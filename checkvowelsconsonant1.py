cfe13040e66de360738320f18552a9291e0e7a08
DHINESH S <37235650+dhinesh22@users.noreply.github.com>
2019-02-05 14:47:56 +0530

    Update and rename checkvowelsconsonant.py to checkvowelsconsonant1.py

---
+++ checkvowelsconsonant1.py
@@ -0,0 +1,10 @@
+original = input('Enter a word:')
+word = original.lower()
+first = word[0]
+if len(original) > 0 and original.isalpha():
+ if(first == ("a" or "e" or "i" or "o" or "u")):
+  print("Vowel")
+ else:
+  print("Consonant")
+else:
+ print("invalid")
