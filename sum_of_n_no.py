bf03abe479ad918f243fb984085af6cf6b3f7f9a
DHINESH S <37235650+dhinesh22@users.noreply.github.com>
2019-06-13 20:58:58 +0530

    Create sum_of_n_no.py

---
+++ sum_of_n_no.py
@@ -0,0 +1,8 @@
+print("Enter the number:")
+i = 1
+total = 0
+n = int(input())
+for i in range(1,n+1):
+    total = total+i
+print(total)                                                                  
+                        
