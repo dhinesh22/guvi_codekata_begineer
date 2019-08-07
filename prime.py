ed6fbd4696f3793e5ad869f3a4cd78e8326661ef
DHINESH S <37235650+dhinesh22@users.noreply.github.com>
2019-08-08 00:49:36 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -3,8 +3,10 @@ l=len(N)
 N=int(N)
 if l<=1000:
     mid =N//2
-    for i in range(1,mid+1):
-        if N%i==0:
+    for i in range(2,mid+1):
+        if N%i!=0:
             print('yes')
         else:
             print('no')
+        break
+    
